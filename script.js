020b2e7084ea98185435cd5d0d395dcc72fae3d9
Jakob <shavinski.jakob@gmail.com>
2023-03-29 19:22:03 -0700

    revamped menu start again button, added best score button, added more cards to game board

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 "use strict";
 
 const FOUND_MATCH_WAIT_MSECS = 1000;
-const picturesArr = [ `img/brian.png`, 'img/britt.png', 'img/elie.png', 'img/kadeem.png', 'img/sarah.png', 'img/brian.png', 'img/britt.png', 'img/elie.png', 'img/kadeem.png', 'img/sarah.png'];
+const picturesArr = [ `img/brian.png`, 'img/britt.png', 'img/elie.png', 'img/kadeem.png', 'img/sarah.png', 'img/brittiany.png', 'img/colt.png', 'img/edmond.png', 'img/joel.png', 'img/kate.png', 'img/spencer.png', 'img/vanesa.png', 'img/whitney.png', 'img/lucy.png', 'img/brian.png', 'img/britt.png', 'img/elie.png', 'img/kadeem.png', 'img/sarah.png', 'img/brittiany.png', 'img/colt.png', 'img/edmond.png', 'img/joel.png', 'img/kate.png', 'img/spencer.png', 'img/vanesa.png', 'img/whitney.png', 'img/lucy.png'];
 
 const startButton = document.querySelector('#start-button');
 const startHeading = document.querySelector('div > h1');
 const gameHeader = document.querySelector('.game-header');
-const gameBoard = document.querySelector('.game')
-const score = document.querySelector('.score')
+const gameBoard = document.querySelector('.game');
+const score = document.querySelector('.score');
+const bestScore = document.querySelector('.best-score');
 const gameOverDisplay = document.querySelector('.game-over');
-const menuButton = document.querySelector('#home-screen')
+const menuButton = document.querySelector('#home-screen');
 
-
-const pictures = shuffle(picturesArr);
+let inProcessOfGuess = false;
+let flippedCard = false;
+let firstCard;
+let secondCard;
+let points = 0;
+let setsFound = 0;
+let highScore;
+let totalSets = picturesArr.length / 2;
 
 function shuffle(items) {
     for (let i = items.length - 1; i > 0; i--) {
@@ -23,7 +30,6 @@ function shuffle(items) {
 }
 
 function createCards(pictures) {
-    const gameBoard = document.querySelector(".game");
     
     for (let picture of picturesArr) {
       const card = document.createElement('div');
@@ -35,17 +41,11 @@ function createCards(pictures) {
       gameBoard.append(card);
   
       (card.addEventListener('click', handleCardClick));
-    };
-  };
 
-  let flippedCard = false;
-  let firstCard;
-  let secondCard;
-  let inProcessOfGuess = false;
-  let points = 0;
-  let setsFound = 0;
-  let totalSets = picturesArr.length / 2;
-
+    }; 
+  }
+ 
+  // once a card is clicked it is flipped to back
   function flipCard(card) {
     card.target.classList.toggle('front')
     card.target.classList.toggle('back')
@@ -53,18 +53,19 @@ function createCards(pictures) {
 
   }
   
+  // if no match then card unflips back to front
   function unFlipCard(card) {
     card.target.classList.toggle('front')
     card.target.classList.toggle('back')
     card.target.style.removeProperty('background-image')
   }
   
-  
+  // handles all of our clicking events
   function handleCardClick(evt) {
     
     if(inProcessOfGuess) {
       return;
-    };
+    }
     
     flipCard(evt);
     
@@ -81,8 +82,8 @@ function createCards(pictures) {
       }
       flippedCard = false;
       inProcessOfGuess = true;
-      addPoint()
-    };
+      addPoint();
+    }
     
     // added this if statement in order to avoid error in console of accessing second.target.id too early
     // would get error of second.target.id is undefined everytime I click first square 
@@ -96,25 +97,34 @@ function createCards(pictures) {
           inProcessOfGuess = false;
           foundSet();
           if (totalSets === setsFound) {
-            console.log('why ARE YOU NOT WORKING');
             gameOver();
-          } else {
-            console.log('no win yet');
           }
         }, FOUND_MATCH_WAIT_MSECS);
-    } else {
-      setTimeout(() => {
-        unFlipCard(firstCard);
-        unFlipCard(secondCard);
-        firstCard = undefined;
-        secondCard = undefined;
-        inProcessOfGuess = false;
-      }, 1000);
-    };
-  };
-  
-};
+      } else {
+        setTimeout(() => {
+          unFlipCard(firstCard);
+          unFlipCard(secondCard);
+          firstCard = undefined;
+          secondCard = undefined;
+          inProcessOfGuess = false;
+        }, 1000);
+      }
+    }
+}
 
+// when click on start button it will automatically load all cards and display the game board
+function startGame() {
+  const pictures = shuffle(picturesArr);
+  createCards(pictures);
+  startButton.style.display = 'none';
+  startHeading.style.display = 'none';
+  gameHeader.style.display = 'flex';
+  gameBoard.style.display = 'flex';
+  score.textContent = points;
+}
+
+// once start button is clicked it will load the game
+startButton.addEventListener('click', startGame)
 
 
 // when a click event happens on a card that has been selected it will add to the score visible on page
@@ -128,25 +138,45 @@ function foundSet() {
   setsFound += 1;
 }
 
-// when click on start button it will automatically load all cards and display the game board
-function startGame() {
-  createCards(pictures);
-  startButton.style.display = 'none'
-  startHeading.style.display = 'none'
-  gameHeader.style.display = 'flex'
-  score.textContent = points;
-};
+// stores best score from playing
+function storeBestScore() {
+  // checks to see if current score is less than stores highscore and replaces 
+  if(score < highScore) {
+    highScore = points;
+    bestScore.textContent = highScore;
+  };
+  
+  highScore = points;
+  bestScore.textContent = highScore;
+}
 
-// once start button is clicked it will load the game
-startButton.addEventListener('click', startGame)
-
-// once sets found is equal to amount of sets the game will end and button will be given to return to menu
+// once sets found is equal to amount of sets the game will end and button will be given to return to menu, should clear existing cards from gameBoard
 function gameOver() {
+  storeBestScore();
+  deleteAllCurrentCards();
   gameBoard.style.display = 'none';
-  gameOverDisplay.style.display = 'flex'
-};
+  gameOverDisplay.style.display = 'flex';
+  points = 0;
+  setsFound = 0;
+}
+
+// clears ending screen when you win and goes back to main menu 
+function clearScreenAndGoToMenu() {
+  startButton.style.display = 'block';
+  startHeading.style.display = 'block';
+  gameHeader.style.display = 'none';
+  gameOverDisplay.style.display = 'none';
+}
 
 // refreshes and gives a new page
-menuButton.addEventListener('click', function() {
-  history.go(0);
-});
+menuButton.addEventListener('click', clearScreenAndGoToMenu);
+
+
+// deletes previous gameBoard filled with cards 
+function deleteAllCurrentCards() {
+  const allActiveCards = gameBoard.querySelectorAll('div');
+
+  for (let card of allActiveCards) {
+    card.remove();
+  };
+}
